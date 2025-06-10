fc8765bbd522478a35938c69021db58c255bb3e5
luki-prsk <tsddka235@gmail.com>
2025-06-10 18:13:23 +0800

    message

---
+++ script.js
@@ -0,0 +1,49 @@
+let goods = [];
+
+fetch('goods.json')
+  .then(res => res.json())
+  .then(data => {
+    goods = data;
+    populateSelects();
+    updateImages();
+  });
+
+function populateSelects() {
+  const character = [...new Set(goods.map(b => b.character))];
+  const category = [...new Set(goods.map(b => b.category))];
+
+  fillSelect('characterSelect', character);
+  fillSelect('categorySelect', category);;
+
+  document.getElementById("characterSelect").addEventListener("change", updateImages);
+  document.getElementById("categorySelect").addEventListener("change", updateImages);
+}
+
+function fillSelect(id, items) {
+  const select = document.getElementById(id);
+  items.forEach(item => {
+    const option = document.createElement("option");
+    option.value = item;
+    option.textContent = item;
+    select.appendChild(option);
+  });
+}
+
+function updateImages() {
+  const character = document.getElementById("topicSelect").value;
+  const category = document.getElementById("authorSelect").value;
+
+  const filtered = books.filter(b =>
+    (!character || b.character === character) &&
+    (!category || b.category === category)
+  );
+
+  const container = document.getElementById("imageContainer");
+  container.innerHTML = "";
+
+  filtered.forEach(b => {
+    const img = document.createElement("img");
+    img.src = b.src;
+    container.appendChild(img);
+  });
+}
